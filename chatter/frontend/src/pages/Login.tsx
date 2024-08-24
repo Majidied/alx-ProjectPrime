4a382b16e4cfdd0b2dbb5dd73688877cbd8038b3
majidied <majidimajidi2003@gmail.com>
2024-08-24 11:42:02 +0100

    feat: Update login page UI and error handling

--- chatter/frontend/src/pages/Login.tsx
+++ chatter/frontend/src/pages/Login.tsx
@@ -2,8 +2,7 @@ import { useEffect, useState } from 'react';
 import { useAuth } from '../hooks/useAuth';
 import { Link, useNavigate } from 'react-router-dom';
 import { login } from '../utils/User';
-import { Input } from '@mui/material';
-import { Button } from '@mui/material';
+import { Input, Button } from '@mui/material';
 import Notification from '../components/Notification/Notification';
 import { AxiosError } from 'axios';
 
@@ -15,30 +14,27 @@ const Login = () => {
     message: '',
     visible: false,
   });
-
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
 
   const handleLogin = async () => {
-    setError(''); // Clear any previous errors
     try {
       const response = await login(email, password);
-      if (response && (response as { token: string }).token) {
-        setToken((response as { token: string }).token);
+      const token = (response as { token: string })?.token;
+      if (token) {
+        setToken(token);
         navigate('/chat', { replace: true });
       } else {
-        setError(
-          'Oops! It looks like your credentials are incorrect. Please try again.'
-        );
+        throw new Error('Invalid credentials');
       }
-    } catch (error) {
-      const errorMessage = (error as AxiosError<{error: string}>)?.response?.data?.error;
-        setError(errorMessage || 'An error occurred. Please try again.');
-        setNotification({
-          type: 'error',
-          message: errorMessage || 'An error occurred. Please try again.',
-          visible: true,
+    } catch (err) {
+      const errorMessage =
+        (err as AxiosError<{ error: string }>)?.response?.data?.error ||
+        'An error occurred. Please try again.';
+      setNotification({
+        type: 'error',
+        message: errorMessage,
+        visible: true,
       });
     }
   };
@@ -46,31 +42,23 @@ const Login = () => {
   useEffect(() => {
     if (notification.visible) {
       const timer = setTimeout(() => {
-        setNotification({ ...notification, visible: false });
+        setNotification((prev) => ({ ...prev, visible: false }));
       }, 5000); // 5 seconds
 
       return () => clearTimeout(timer); // Clean up the timer if the component unmounts
     }
-  }, [notification, notification.visible]);
+  }, [notification]);
 
   return (
-    <div
-      className="flex flex-col items-center justify-center h-screen bg-amber-100"
-      style={{ backgroundImage: 'linear-gradient(to right, #7FA1C3, #4E31AA)' }}
-    >
+    <div className="flex flex-col items-center justify-center h-screen bg-amber-100"
+         style={{ backgroundImage: 'linear-gradient(to right, #7FA1C3, #4E31AA)' }}>
       <section className="bg-white flex flex-col md:flex-row items-center justify-center h-3/4 rounded-lg backdrop-blur-md">
-        <div className="flex-col items-center justify-center bg-white p-8 rounded-lg shadow-lg w-full md:w-1/2 h-full">
+        <div className="flex flex-col items-center justify-center bg-white p-8 rounded-lg shadow-lg w-full md:w-1/2 h-full">
           {/* Mobile view with background image */}
-          <div
-            className="bg-cover bg-center h-64 flex flex-col items-center justify-center rounded-lg md:hidden"
-            style={{ backgroundImage: `url('src/assets/login.jpg')` }}
-          >
+          <div className="bg-cover bg-center h-64 flex flex-col items-center justify-center rounded-lg md:hidden"
+               style={{ backgroundImage: `url('src/assets/login.jpg')` }}>
             <h1 className="text-4xl font-bold mb-4 bg-gradient-to-r from-cyan-600 to-violet-900 bg-clip-text text-transparent">
-              Welcome{' '}
-              <span className="bg-gradient-to-r from-violet-900 to-cyan-600 bg-clip-text text-transparent">
-                back
-              </span>
-              !
+              Welcome back!
             </h1>
           </div>
           <h1 className="text-4xl font-bold mb-4 hidden md:block">Login</h1>
@@ -78,10 +66,10 @@ const Login = () => {
           <div className="w-full p-4">
             {notification.visible && (
               <Notification
-                type={error ? 'error' : 'success'}
+                type="error"
                 message={notification.message}
                 onClose={() =>
-                  setNotification({ ...notification, visible: false })
+                  setNotification((prev) => ({ ...prev, visible: false }))
                 }
               />
             )}
@@ -122,7 +110,7 @@ const Login = () => {
           </p>
         </div>
         <div
-          className="bg-cover bg-center p-8 rounded-lg shadow-lg w-full md:w-1/2 h-full flex-col justify-center items-center text-center hidden md:flex"
+          className="bg-cover bg-center p-8 rounded-lg shadow-lg w-full md:w-1/2 h-full flex flex-col justify-center items-center text-center hidden md:flex"
           style={{ backgroundImage: `url('src/assets/login.jpg')` }}
         >
           <h1 className="text-4xl font-bold text-black mb-4 bg-gradient-to-r from-cyan-600 to-violet-900 bg-clip-text text-transparent">
